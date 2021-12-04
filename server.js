200d2827a460b60932749802aef27758dccff599
Jezer Chilel <jezer.chilel@gmail.com>
2021-12-04 11:38:01 -0600

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+
+app.user(bodyParser.json())
 
 const mockUserData = [
 	{ name: 'Mark' },
@@ -20,6 +24,27 @@ app.get('/users/:id', (req, res) => {
 		message: 'got one user',
 		users: req.params.id
 	})
+})
+
+app.post('/login', (req, res) => {
+	const username = req.body.username
+	const password = req.body.password
+
+	const mockUsername = 'billyTheKid'
+	const mockPassword = 'superSecret'
+
+	if (username === mockUsername && password === mockPassword) {
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
 })
 
 app.listen(8000, () => {
